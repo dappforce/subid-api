782d881fac3104ae2a40fd9a58b58779dba71763
Mell <oleh.melnechyk@gmail.com>
2023-12-27 14:52:22 +0200

    Add Grill to cors policy

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,6 +1,6 @@
 export const reqTimeoutSecs = 60
 
-export const allowedOrigins = [ 'sub.id', 'polkaverse.com', 'subsocial.network' ] // process.env.CORS_ALLOWED_ORIGIN?.split(',').map(x => x.trim()) || ['http://localhost']
+export const allowedOrigins = [ 'sub.id', 'polkaverse.com', 'subsocial.network', 'grill.chat' ] // process.env.CORS_ALLOWED_ORIGIN?.split(',').map(x => x.trim()) || ['http://localhost']
 
 export const port = process.env.OFFCHAIN_SERVER_PORT || 3001
 
